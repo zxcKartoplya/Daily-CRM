825543456c5a40dbc3efccf9c0f090824a55579d
Kirill <bel-kir05@mail.ru>
2026-04-06 00:02:01 +0800

    fix allowed origins

--- app/main.py
+++ app/main.py
@@ -16,6 +16,7 @@ def create_app() -> FastAPI:
         allow_origins=[
             "http://localhost:3000",
             "http://127.0.0.1:3000",
+            "http://178.253.45.236:3000"
         ],
         allow_credentials=True,
         allow_methods=["*"],
